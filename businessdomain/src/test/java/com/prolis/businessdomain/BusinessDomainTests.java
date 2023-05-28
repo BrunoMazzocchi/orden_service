93c7f21b601b1ebfa716af64b90ef53ac6a98bd5
Bruno Mazzocchi <brunomazzocchidev@gmail.com>
2023-05-28 11:59:45 -0600

    Added business model files

---
+++ businessdomain/src/test/java/com/prolis/businessdomain/BusinessDomainTests.java
@@ -0,0 +1,13 @@
+package com.prolis.businessdomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessDomainTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
