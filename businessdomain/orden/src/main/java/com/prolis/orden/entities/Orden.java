11c067018d5e8cbdc6ef62e45cb617fa87f1c39b
Joshdroid <gutierrez25g@gmail.com>
2023-05-28 15:09:51 -0600

    Correccion en el nombre de los campos en la entidad de orden

--- businessdomain/orden/src/main/java/com/prolis/orden/entities/Orden.java
+++ businessdomain/orden/src/main/java/com/prolis/orden/entities/Orden.java
@@ -1,9 +1,9 @@
 package com.prolis.orden.entities;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Data;
 
-import java.sql.*;
+import java.util.Date;
 
 @Entity
 @Data
@@ -11,6 +11,51 @@ import java.sql.*;
 public class Orden {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "IdOrden")
+    @Column(name = "idorden",  nullable = false, unique = true)
     private int idOrden;
+
+    @Column(name = "N_Orden", length = 11)
+    private String numeroOrden;
+
+    @Column(name = "idempleado", nullable = false, unique = true)
+    private int idEmpleado;
+
+    @Column(name = "idpaciente", nullable = false, unique = true)
+    private int idPaciente;
+
+    @Column(name = "idtiposervicio", nullable = false, unique = true)
+    private int idTipoServicio;
+
+    @Column(name = "idtipoorden", nullable = false, unique = true)
+    private int idTipoOrden;
+
+    @Column(name = "Asistencia", length = 1)
+    private String asistencia;
+
+    @Column(name = "Observaciones", length = 200)
+    private String observaciones;
+
+    @Column(name = "fechaorden", nullable = false, unique = true)
+    private Date fechaOrden;
+
+    @Column(name = "Activo", length = 1)
+    private String activo;
+
+    @Column(name = "fechaimprime", nullable = false, unique = true)
+    private Date fechaImprime;
+
+    @Column(name = "idusuarioimprime", nullable = false, unique = true)
+    private int idUsuarioImprime;
+
+    @Column(name = "Estado")
+    private int estado;
+
+    @Column(name = "Finalizado", length = 1)
+    private String finalizado;
+
+    @Column(name = "fechacita", nullable = false, unique = true)
+    private Date fechaCita;
+
+    @Column(name = "fechapreporte", nullable = false, unique = true)
+    private Date fechaPReporte;
 }
